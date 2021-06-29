8f156249d47ee22de32e90a5e866ebc4b53522df
Christian Santana <christians1121@gmail.com>
2021-06-29 19:27:24 +0000

    formatted json to show readable data

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,7 +1,21 @@
 import requests
+import json
 
 url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 
 response = requests.get(url)
 
-print(response.json())
+Data = response.json()
+disc = Data['disclaimer']
+crypto = Data['chartName']
+times = Data['time']
+time = times['updated']
+cur = Data['bpi']
+USDollar = cur['USD']
+USD = USDollar['rate']
+EUR = cur['EUR']
+EURO = EUR['rate']
+print(f'{disc}')
+print(f'Current price of {crypto} at {time}:')
+print(f'United States Dollars(USD): ${USD}')
+print(f'Euros(EUR): €{EURO}')
