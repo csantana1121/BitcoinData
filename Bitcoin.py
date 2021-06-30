cca178f518032036a98e3d2c2b12944730646adb
Christian Santana <christians1121@gmail.com>
2021-06-30 18:57:44 +0000

    style corrections

--- Bitcoin.py
+++ Bitcoin.py
@@ -8,28 +8,28 @@ from sqlalchemy import create_engine
 # Test 2: Check that Data object is type json
 # Test 3: Check that Data is not empty
 def getBitcoindata():
-  url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-  response = requests.get(url)
-  Data = response.json()
-  return Data
+    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    response = requests.get(url)
+    Data = response.json()
+    return Data
 
 # Test 1: Check that currentvalues list is not empty
 # Test 2: Check that currentvalues list has 4 elements and not null
 # Test 3: Check that time is a date and other 3 vars are ints.
 def parseJson(Data):
-  disc = Data['disclaimer']
-  crypto = Data['chartName']
-  times = Data['time']
-  time = times['updated']
-  cur = Data['bpi']
-  USDollar = cur['USD']
-  USD = USDollar['rate']
-  EUR = cur['EUR']
-  EURO = EUR['rate']
-  Pounds = cur['GBP']
-  GBP = Pounds['rate']
-  currentvalues =[time, USD, EURO, GBP]
-  return currentvalues
+    disc = Data['disclaimer']
+    crypto = Data['chartName']
+    times = Data['time']
+    time = times['updated']
+    cur = Data['bpi']
+    USDollar = cur['USD']
+    USD = USDollar['rate']
+    EUR = cur['EUR']
+    EURO = EUR['rate']
+    Pounds = cur['GBP']
+    GBP = Pounds['rate']
+    currentvalues = [time, USD, EURO, GBP]
+    return currentvalues
 
 # print(f'{disc}')
 # print(f'Current price of {crypto} at {time}:')
@@ -39,18 +39,19 @@ def parseJson(Data):
 # print(df)
 
 # Test 1: Check that dataframe is a 1 row 4 column table
-# Test 2: Check that parseJson function has worked and return a list of 4 values in order
-# Test 3: Check that sql database bitcoin exists 
+# Test 2: Check that parseJson function has worked and return a list of 4
+# values in order
+# Test 3: Check that sql database bitcoin exists
 # Test 4: Check that table Price_Index in bitcoin is made and not empty
-# Test 5: Check that Price_Index table is not overwritten and adds new value each call
+# Test 5: Check that Price_Index table is not overwritten and adds new
+# value each call
 def addtoPriceIndex():
-  col_names = ['Date', 'USD', 'EURO', 'GBP']
-  df = pd.DataFrame(columns=col_names)
-  df.loc[len(df.index)] = parseJson(Data)
-  engine = create_engine('mysql://root:codio@localhost/bitcoin')
-  df.to_sql('Price_Index', con=engine, if_exists='append', index=False)
+    col_names = ['Date', 'USD', 'EURO', 'GBP']
+    df = pd.DataFrame(columns=col_names)
+    df.loc[len(df.index)] = parseJson(Data)
+    engine = create_engine('mysql://root:codio@localhost/bitcoin')
+    df.to_sql('Price_Index', con=engine, if_exists='append', index=False)
 
-  
 if __name__ == "__main__":
-  Data = getBitcoindata()
-  addtoPriceIndex()
+    Data = getBitcoindata()
+    addtoPriceIndex()
