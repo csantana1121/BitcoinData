8be949ceafec255b89aa55763ebd5f4713c9b97d
Christian Santana <christians1121@gmail.com>
2021-06-30 17:41:29 +0000

    Add bitcoin price data to sql table

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,6 +1,8 @@
 import requests
 import json
-
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
 url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 
 response = requests.get(url)
@@ -15,7 +17,17 @@ USDollar = cur['USD']
 USD = USDollar['rate']
 EUR = cur['EUR']
 EURO = EUR['rate']
-print(f'{disc}')
-print(f'Current price of {crypto} at {time}:')
-print(f'United States Dollars(USD): ${USD}')
-print(f'Euros(EUR): €{EURO}')
+Pounds = cur['GBP']
+GBP = Pounds['rate']
+#print(f'{disc}')
+#print(f'Current price of {crypto} at {time}:')
+#print(f'United States Dollars(USD): ${USD}')
+#print(f'Euros(EUR): €{EURO}')
+#print(f'British pound sterlings(GBP): £{GBP}')
+
+col_names = ['Date', 'USD', 'EURO', 'GBP']
+df = pd.DataFrame(columns = col_names)
+df.loc[len(df.index)] = [time, USD, EURO, GBP]
+#print(df)
+engine = create_engine('mysql://root:codio@localhost/bitcoin')
+df.to_sql('Price_Index', con = engine, if_exists='append',index=False)
