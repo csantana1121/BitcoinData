421d9efd2432f30a40d6305544152bda1b3c4bc4
Christian Santana <christians1121@gmail.com>
2021-06-30 17:47:59 +0000

    style guide fixes 3

--- Bitcoin.py
+++ Bitcoin.py
@@ -20,15 +20,15 @@ EURO = EUR['rate']
 Pounds = cur['GBP']
 GBP = Pounds['rate']
 
-#print(f'{disc}')
-#print(f'Current price of {crypto} at {time}:')
-#print(f'United States Dollars(USD): ${USD}')
-#print(f'Euros(EUR): €{EURO}')
-#print(f'British pound sterlings(GBP): £{GBP}')
+# print(f'{disc}')
+# print(f'Current price of {crypto} at {time}:')
+# print(f'United States Dollars(USD): ${USD}')
+# print(f'Euros(EUR): €{EURO}')
+# print(f'British pound sterlings(GBP): £{GBP}')
 
 col_names = ['Date', 'USD', 'EURO', 'GBP']
-df = pd.DataFrame(columns= col_names)
+df = pd.DataFrame(columns=col_names)
 df.loc[len(df.index)] = [time, USD, EURO, GBP]
-#print(df)
+# print(df)
 engine = create_engine('mysql://root:codio@localhost/bitcoin')
 df.to_sql('Price_Index', con = engine, if_exists = 'append', index=False)
